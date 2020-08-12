5faecd83b3bd645eefd13015af46f1a644a9cd53
MaxRojek <50421779+MaxRojek@users.noreply.github.com>
2020-08-12 23:22:20 +0200

    changes in Card.js

--- src/Components/Localizations/Card.js
+++ src/Components/Localizations/Card.js
@@ -2,11 +2,7 @@ import React from "react";
 
 import "./Card.css";
 const SpotCard = (props) => {
-  return (
-    <div className="flex card shadow-5">
-      <a>{props.title}</a>
-    </div>
-  );
+  return <div className="flex card shadow-5">{props.title}</div>;
 };
 
 export default SpotCard;
